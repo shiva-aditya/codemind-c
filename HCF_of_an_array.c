d833a5eca464b93fbbe66f0ccd223663a417dd00
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-10-30 22:21:53 +0530

    #include<bits/stdc++.h> using namespace std; bool divide(vector <int> fact , int s){     for (auto it : fact){         if (it%s!=0)return false;     }     return true; } int main(){     int t;     cin>>t;     vector<int>vec;     for (int i=0 ; i<t ;i++){         int y;         cin>>y;         vec.push_back(y);     }     int min=*min_element(vec.begin(),vec.end());     vector<int>fact;     for (int i=1 ; i<min+1 ; i++){         if (min%i==0)fact.push_back(i);     }     for (int i=fact.size()-1 ; i>-1 ; i--){         if (divide(vec,fact[i])){             cout<<fact[i];             break;         }     } }

---
+++ HCF_of_an_array.c
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool divide(vector <int> fact , int s){
+    for (auto it : fact){
+        if (it%s!=0)return false;
+    }
+    return true;
+}
+int main(){
+    int t;
+    cin>>t;
+    vector<int>vec;
+    for (int i=0 ; i<t ;i++){
+        int y;
+        cin>>y;
+        vec.push_back(y);
+    }
+    int min=*min_element(vec.begin(),vec.end());
+    vector<int>fact;
+    for (int i=1 ; i<min+1 ; i++){
+        if (min%i==0)fact.push_back(i);
+    }
+    for (int i=fact.size()-1 ; i>-1 ; i--){
+        if (divide(vec,fact[i])){
+            cout<<fact[i];
+            break;
+        }
+    }  
+}
