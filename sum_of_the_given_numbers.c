a097fcf3b096d0a5094cc6d1ab6fe449c9b0cab7
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-11-01 22:40:45 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int y;     cin>>y;     for (int i=0 ; i<y ; i++){         int p,l;         cin>>p>>l;         cout<<p+l<<endl;     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int y;
+    cin>>y;
+    for (int i=0 ; i<y ; i++){
+        int p,l;
+        cin>>p>>l;
+        cout<<p+l<<endl;
+    }
+}
