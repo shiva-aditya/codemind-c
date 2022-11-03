2c8a108f01f57c59004ca385209c489f269519e5
shiva-aditya <109405657+shiva-aditya@users.noreply.github.com>
2022-11-03 15:01:00 +0530

    #include<iostream> using namespace std; int main(){     int a,b,c;     cin>>a>>b>>c;     if (a>50 and b>60 and c>100)cout<<10;     else if (a>50 and b>60 and c<=100)cout<<9;     else if (a<=50 and b>60 and c>100)cout<<8;     else if (a>50 and b<=60 and c>100)cout<<7;     else if (a>50 or b>60 or c>100)cout<<6;     else cout<<5; }

---
+++ Grades_of_Rides.c
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if (a>50 and b>60 and c>100)cout<<10;
+    else if (a>50 and b>60 and c<=100)cout<<9;
+    else if (a<=50 and b>60 and c>100)cout<<8;
+    else if (a>50 and b<=60 and c>100)cout<<7;
+    else if (a>50 or b>60 or c>100)cout<<6;
+    else cout<<5;
+}
