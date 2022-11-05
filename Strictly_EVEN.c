63e24b02675300562f34820c8064ed6e63af48c0
Shiva K <109405657+shiva-aditya@users.noreply.github.com>
2022-11-05 15:06:28 +0530

    #include<bits/stdc++.h> using namespace std; bool p(vector<int>a, int l){     for (int i=0 ; i<l ; i++){         if (a[i]%2==0){             if (i%2!=0)return false;         }     }     return true; } int main(){     int s;     cin>>s;     vector<int>q;     for (int i=0 ; i<s ; i++){         int r;         cin>>r;         q.push_back(r);     }     if(p(q,s))cout<<"True";     else cout<<"False"; }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool p(vector<int>a, int l){
+    for (int i=0 ; i<l ; i++){
+        if (a[i]%2==0){
+            if (i%2!=0)return false;
+        }
+    }
+    return true;
+}
+int main(){
+    int s;
+    cin>>s;
+    vector<int>q;
+    for (int i=0 ; i<s ; i++){
+        int r;
+        cin>>r;
+        q.push_back(r);
+    }
+    if(p(q,s))cout<<"True";
+    else cout<<"False";
+}
