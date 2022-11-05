6ff43e1f7f3bd352a9ad00430b497eab5accba52
Shiva K <109405657+shiva-aditya@users.noreply.github.com>
2022-11-06 01:40:05 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int t;     cin>>t;     long long s=0;     for (int i=0 ; i<t ; i++){         long long a;         cin>>a;         s+=a;     }     cout<<s; }

---
+++ A_Very_Big_Sum.c
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    long long s=0;
+    for (int i=0 ; i<t ; i++){
+        long long a;
+        cin>>a;
+        s+=a;
+    }
+    cout<<s;
+}
